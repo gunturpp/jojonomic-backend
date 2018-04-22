00692272268ce413b6ff52b98af6f6dce12da61e
mluthfirr <muhammad_luthfirrahman@apps.ipb.ac.id>
2018-04-22 21:16:28 +0700

    Lumen Exercise

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        
+        return Board::all();
+    
+    }
+
+    public function show($boardId)
+    {
+        $board=Board::findOrFail($boardId);
+        return $board;
+    
+    }
+
+    public function store (Request $request)
+    {
+        
+        Board::create([
+            'name' => $request->name,
+            'user_id' => $request->user_id,
+
+        ]);
+        
+        return response()->json(['message'=>'success'],200);
+    }
+}
